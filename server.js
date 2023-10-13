72dd7b7c056f8fc2375998d1345df95187109065
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-13 12:55:11 +0900

    수정 후 서버 구동 확인.

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const HTMLTemplate = require('/Users/ms/Desktop/VSCODE/my_project_callinder-1/se
 const contentType = {'contentType' : 'text/html; charset=utf-8'};
 
 http.createServer(function(req, res) {
+  //서버를 구동시키는 if문
   if(req.method === 'GET' && req.url === '/') {
-
   res.writeHead(200, contentType);
-  fs.readFile('login.html',(err,data) => {
+  //readFile을 작업 할 경우 git 폴더 안 폴더도 경로 설정을 지정해줘야 한다.
+  fs.readFile('./Login/login.html',(err,data) => {
+    //파일을 불러 올 경우의 에러를 표시해주는 if문
     if(err) {
       console.log('파일을 불러 올 수 없습니다.')
+    
     } else {
       res.end(data)
     }
