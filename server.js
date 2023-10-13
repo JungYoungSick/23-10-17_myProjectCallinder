9ff81cbdd3245337c4bf3e40b26d8f002259852e
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-13 12:43:59 +0900

    서버 1차 작성 완료

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const http = require('http');
 const fs = require('fs')
+const HTMLTemplate = require('./model/HTML-model')
 
-const contentType = {
-  'contentType' : 'text/html; charset=utf-8'};
+const contentType = {'contentType' : 'text/html; charset=utf-8'};
 
-// http.createServer(function(req, res) {
+http.createServer(function(req, res) {
+  if(req.method === 'GET' && req.url === '/') {
+
+  res.writeHead(200, contentType);
+  fs.readFile('login.html',(err,data) => {
+    if(err) {
+      console.log('파일을 불러 올 수 없습니다.')
+    } else {
+      res.end(data)
+    }
+  })
   
+} else {
+    res.writeHead(404, contentType);
+    res.end("<h1>해당 페이지를 찾을 수 없습니다.</h1>")
+  }
+})
+server.listen(8080); 
 
-//   //작성법 이유 알아보기.(검색 해보기)
-
-//   res.writeHead(200,Login);
-//   res.end()
-// }).listen(1010); 
-
-let Main = {}
-let sub = {}
+// let Main = {}
+// let sub = {}
